b26012a20ed6580383303fa1fde6552dd081029a
DevOpsTerraformLabs <ghanshammahajan29@gmail.com>
2025-12-12 16:50:07 +0530

    Azure Resource Group Module

--- example/simple/main.tf
+++ example/simple/main.tf
@@ -1,19 +1,22 @@
-module "rg_local" {
-  source   = "../../"
-  name     = "rg-example-local"
+#Local Usage example
+
+#module "rg_local" {
+#  source   = "../../"
+#  name     = "rg-example-local"
+#  location = "East US"
+#  tags = {
+#    owner = "DevOpsTerraformLabs"
+#    env   = "Dev"
+#  }
+#}
+
+#Remote Usage example. Consumers will use this form:
+module "rg_remote" {
+  source   = "git@github.com:DevOpsTerraformLabs/terraform-azurerm-resourcegroup.git?ref=v1.0.0"
+  name     = "rg-example-remote"
   location = "East US"
   tags = {
     owner = "DevOpsTerraformLabs"
     env   = "Dev"
   }
 }
-//Remote Usage example (Commented). Consumers will use this form:
-//module "rg_remote" {
-//  source   = "git@github.com:DevOpsTerraformLabs/terraform-azurerm-resourcegroup.git"
-//  name     = "rg-example-remote"
-//  location = "East US"
-//  tags = {
-//    owner = "DevOpsTerraformLabs"
-//    env   = "Dev"
-//  }
-//}
